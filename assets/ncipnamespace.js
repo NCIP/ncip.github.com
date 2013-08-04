e8699cfe8a4ff3fdced0444d7d7ca0c612a8a1e5
Luis Ibanez <luis.ibanez@kitware.com>
2013-08-03 21:37:17 -0400

    Fixed the logic for conditional request.
    Using the If-modified-since header option.

---
+++ assets/ncipnamespace.js
@@ -0,0 +1,64 @@
+/// ncipnamespace
+///
+/// Copyright 2013 Kitware Inc.
+/// Apache 2.0 License
+///
+/// Writen after example from "JavaScript Patterns", pages 89-90.
+///
+
+window.NCIPGlobal = (function () {
+
+  var that = {};
+
+  that.namespace = function(ns_string) {
+
+    var parts = ns_string.split('.'),
+        parent = NCIPGlobal,
+        i;
+
+    // strip redundant leading global
+    if (parts[0] === "NCIPGlobal" ) {
+      parts = parts.slice(1);
+      }
+
+    for (i = 0; i < parts.length; i += 1) {
+      // create a property if it doesn't exist
+      if (typeof parent[parts[i]] === "undefined") {
+        parent[parts[i]] = {};
+        }
+      // insert the property in a nested pattern
+      parent = parent[parts[i]];
+      }
+
+    return parent;
+
+    };
+
+  that.getJSONIfModified = function(uri,successFunction) {
+
+    NCIPGlobal.namespace('repos');
+    NCIPGlobal.namespace('cache.date');
+
+    NCIPGlobal.cache.date = window.localStorage.getItem('NCIPDate');
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('get',uri,false);
+    xhr.setRequestHeader('If-Modified-Since',NCIPGlobal.cache.date);
+    xhr.send(null);
+
+    var result = {};
+
+    result.data = xhr.responseText;
+    result.status = xhr.status;
+    result.responseHeaders = xhr.getAllResponseHeaders();
+
+    console.log('xhr result');
+    console.log(result);
+
+    successFunction(result);
+    };
+
+  return that;
+
+}());
